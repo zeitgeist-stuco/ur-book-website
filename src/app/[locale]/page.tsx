249990757b73da862ebc3be05a503a33acc076c6
Henri <henri@ur-book.com>
2026-04-09 17:40:20 +0200

    feat: wire up Testimonials + StickyBar in page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,8 +3,10 @@ import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 import { HowItWorks } from '@/components/HowItWorks'
 import { Pricing } from '@/components/Pricing'
+import { Testimonials } from '@/components/Testimonials'
 import { FAQ } from '@/components/FAQ'
 import { Footer } from '@/components/Footer'
+import { StickyBar } from '@/components/StickyBar'
 
 export default function HomePage() {
   return (
@@ -14,9 +16,11 @@ export default function HomePage() {
         <Hero />
         <HowItWorks />
         <Pricing />
+        <Testimonials />
         <FAQ />
       </main>
       <Footer />
+      <StickyBar />
     </>
   )
 }
